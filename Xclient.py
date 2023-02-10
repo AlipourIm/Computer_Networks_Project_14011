4b6bb521935f12e791505c45f757e5e3a312550f
AlipourIm <iman.alip2001@gmail.com>
2023-02-11 00:20:49 +0330

    add robustness to our `Xserver` and 'Xclient'

--- Xclient.py
+++ Xclient.py
@@ -9,10 +9,13 @@ import ssl
 from PrettyLogger import logger_config
 import Constants
 import threading
+import time
 
 
 from PrettyLogger import logger_config
 log = logger_config("webserver")
+
+xserver_socket: socket.socket
 
 '''
 
@@ -24,22 +27,33 @@ with socket.create_connection((hostname, 443)) as sock:
 '''
 
 def establish_HTTPS_connection() -> socket.socket:
-    hostname = 'localhost'
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.load_verify_locations("./keys/certificate.pem")
+    global xserver_socket
+    sleep_time = 1
+    while True:
+        try:
+            hostname = 'localhost'
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            context.load_verify_locations("./keys/certificate.pem")
 
-    '''
-    # Use two lines below instead of line above if you don't want to check self-signed certificate 
-    context.verify_mode = ssl.CERT_NONE 
-    context.check_hostname = False
-    '''
+            '''
+            # Use two lines below instead of line above if you don't want to check self-signed certificate 
+            context.verify_mode = ssl.CERT_NONE 
+            context.check_hostname = False
+            '''
 
-    raw_sock = socket.create_connection((hostname, 443))
-    https_sock = context.wrap_socket(raw_sock, server_hostname = Constants.X_SERVER_DOMAIN_NAME)
-    
-    return https_sock
+            raw_sock = socket.create_connection((hostname, 443))
+            https_socket = context.wrap_socket(raw_sock, server_hostname = Constants.X_SERVER_DOMAIN_NAME)
+            xserver_socket = https_socket
+        
+            log.info("Conected to Xserver successfully.")
+            return https_socket
+        except ConnectionRefusedError:
+            log.warning(f"Xserver is not responding... retrying in {sleep_time}")
+            time.sleep(sleep_time)
+            sleep_time *= 2
 
-def client_handler(https_socket: socket.socket):
+def client_handler():
+    global xserver_socket
     UDP_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     UDP_server_socket.bind(("localhost", Constants.XCLIENT_UDP_PORT))
     
@@ -47,21 +61,44 @@ def client_handler(https_socket: socket.socket):
         bytes_address_pair = UDP_server_socket.recvfrom(Constants.BUFFER_SIZE)
         message = bytes_address_pair[0].decode("ascii")
         https_message = f"PUT / HTTP/1.1\r\nHost: localhost\r\nContent-Type: application/zip\r\nContent-Length: {len(message)}\r\n\r\n{message}"
-        https_socket.sendall(https_message.encode("ascii"))
+        xserver_socket.sendall(https_message.encode("ascii"))
 
 
-def xserver_handler(https_socket: socket.socket):
-    pass
+def xserver_handler():
+    global xserver_socket
+    try:
+		# Create a UDP socket at server side, (ipv4, UDP)
+        UDP_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+        while True:
+            buffer = xserver_socket.recv(Constants.BUFFER_SIZE).decode("ascii")
+            log.info(f"message from xserver: {buffer.encode('ascii')}")
+
+            arr = buffer.split("\r\n\r\n", maxsplit=1)
+            https_header = arr[0]
+            UDP_message = arr[1]
+
+			# Send to client using created UDP socket
+            bytesToSend = str.encode(UDP_message)
+            UDP_client_socket.sendto(bytesToSend, ("127.0.0.1", Constants.CLIENT_PORT))
+			
+    except KeyboardInterrupt:
+        UDP_client_socket.close()
+        log.info(f"XServer disconnected.")
+    except IndexError:
+        log.info(f"connection with server failed.")
+        establish_HTTPS_connection()
+        xserver_handler()   # Keep Xserver handler thread alive
 
 if __name__ == "__main__":
     https_socket = establish_HTTPS_connection()
 
     # This thread listens on XCLIENT_UDP_PORT and forwards incomming packets to XSERVER after adding header
-    client_handler_thread = threading.Thread(target=client_handler, args=(https_socket, ))
+    client_handler_thread = threading.Thread(target=client_handler, args=())
     client_handler_thread.start()
 
     # This thread reads from HTTPS socket and forwards incomming packets to CLIENT_PORT after removing custom header
-    xserver_handler_thread = threading.Thread(target=xserver_handler, args=(https_socket, ))
+    xserver_handler_thread = threading.Thread(target=xserver_handler, args=())
     xserver_handler_thread.start()
 
 
--- Xserver.py
+++ Xserver.py
@@ -3,22 +3,22 @@ import ssl
 import threading
 from PrettyLogger import logger_config
 import Constants
+from email.utils import formatdate
+
 
 
 log = logger_config("webserver")
+
+xclient_socket: socket.socket
+UDP_socket: socket.socket
 
 def xclient_handler(client, address):
 	log.info(f"Client with address {address} connected.")
 
 	try:
-		# Create a UDP socket at client side, (ipv4, UDP)
-		UDP_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-		UDP_server_socket.bind(("127.0.0.1", Constants.XSERVER_UDP_PORT))
-
 		while True:
 			buffer = client.recv(Constants.BUFFER_SIZE).decode("ascii")
 			log.info(f"message from xclient: {buffer.encode('ascii')}")
-			client.send("HTTP/1.1 200 OK".encode("ascii"))
 
 			arr = buffer.split("\r\n\r\n", maxsplit=1)
 			https_header = arr[0]
@@ -26,10 +26,9 @@ def xclient_handler(client, address):
 
 			# Send to server using created UDP socket
 			bytesToSend = str.encode(UDP_message)
-			UDP_server_socket.sendto(bytesToSend, ("127.0.0.1", Constants.SERVER_PORT))
+			UDP_socket.sendto(bytesToSend, ("127.0.0.1", Constants.SERVER_PORT))
 			
-	except KeyboardInterrupt:
-		UDP_server_socket.close()
+	except KeyboardInterrupt or IndexError:
 		client.close()
 		log.info(f"Client with address {address} disconnected.")
 
@@ -51,14 +50,30 @@ def establish_https_connection() -> socket.socket:
 	return server_socket
 
 def https_client_handler(https_socket: socket.socket):
+	global xclient_socket
 	try:
 		while True:
 			client, address = https_socket.accept()
+			xclient_socket = client
 			handler_thread = threading.Thread(target=xclient_handler, args=(client, address))
 			handler_thread.start()
 	except KeyboardInterrupt:
 		log.warning("terminating server")
 		https_socket.close()
+
+def server_handler():
+	global xclient_socket, UDP_socket
+	UDP_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+	UDP_server_socket.bind(("localhost", Constants.XSERVER_UDP_PORT))
+	UDP_socket = UDP_server_socket
+    
+	while True:
+		bytes_address_pair = UDP_server_socket.recvfrom(Constants.BUFFER_SIZE)
+		message = bytes_address_pair[0].decode("ascii")
+		https_message = f"HTTP/1.1 200 OK\r\nDate: {formatdate(timeval=None, localtime=False, usegmt=True)}\
+		\r\nContent-Type: application/zip\r\nContent-Length: {len(message)}\r\n\r\n{message}"
+		log.info(f"message to xclient: {https_message.encode('ascii')}")
+		xclient_socket.sendall(https_message.encode("ascii"))
 
 if __name__ == "__main__":
 	https_socket = establish_https_connection()
@@ -66,3 +81,7 @@ if __name__ == "__main__":
 	# This thread reads from HTTPS socket and forwards incomming packets to SERVER_PORT after removing custom header
 	xclient_handler_thread = threading.Thread(target=https_client_handler, args=(https_socket, ))
 	xclient_handler_thread.start()
+
+	# This thread listens on XSERVER_UDP_PORT and forwards incomming packets to XCLIENT after adding header
+	server_handler_thread = threading.Thread(target=server_handler, args=())
+	server_handler_thread.start()
